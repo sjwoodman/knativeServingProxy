0446988c90f3c4c0a050e6c151895e7790da2821
Simon Woodman <swoodman@redhat.com>
2018-10-25 11:44:01 +0100

    Refactoring and testing

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -15,14 +15,14 @@ import (
 
 func main() {
 
-	kafkaConfig := config.GetConfig()
-
-	kafkaConfig.BootStrapServers = os.Getenv("INPUT_DATA_BOOTSTRAP_SERVERS")
+	//Setup the input
+	inputConfig := config.GetConfig()
+	inputConfig.BootStrapServers = os.Getenv("INPUT_DATA_BOOTSTRAP_SERVERS")
+	inputConfig.KafkaTopic = os.Getenv("INPUT_DATA")
 
 	log.Printf("BOOTSTRAP_SERVERS: %s", os.Getenv("INPUT_DATA_BOOTSTRAP_SERVERS"))
 
-
-	consumer, err := sarama.NewConsumer([]string{kafkaConfig.BootStrapServers}, nil)
+	consumer, err := sarama.NewConsumer([]string{inputConfig.BootStrapServers}, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -35,7 +35,7 @@ func main() {
 
 	// OffsetNewest
 	initialOffset := sarama.OffsetOldest //get offset for the oldest message on the topic
-	partitionConsumer, err := consumer.ConsumePartition(os.Getenv("INPUT_DATA"), 0, initialOffset)
+	partitionConsumer, err := consumer.ConsumePartition(inputConfig.KafkaTopic, 0, initialOffset)
 	if err != nil {
 		panic(err)
 	}
@@ -54,50 +54,17 @@ func main() {
 
 	log.Printf("Connected to %s", os.Getenv("INPUT_DATA"))
 
-ConsumerLoop:
-	for {
-		select {
-		case msg := <-partitionConsumer.Messages():
-
-			req, err := http.NewRequest("POST", "http://192.168.64.32:32380", bytes.NewBuffer(msg.Value))
-			req.Host = "helloworld-go.myproject.example.com"
-
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			if err != nil {
-				panic(err)
-			}
-			defer resp.Body.Close()
-
-			fmt.Println("response Status:", resp.Status)
-			fmt.Println("response Headers:", resp.Header)
-			body, _ := ioutil.ReadAll(resp.Body)
-			fmt.Println("response Body:", string(body))
-
-			log.Printf("Consumed message offset %s", msg.Value)
-			consumed++
-
-			sendResponse(body)
-		case <-signals:
-			break ConsumerLoop
-		}
-	}
-
-	log.Printf("Consumed: %d\n", consumed)
-}
-
-func sendResponse(payload []byte) {
-
+	//Setup Output
 	cfg := sarama.NewConfig()
 	cfg.Producer.RequiredAcks = sarama.WaitForAll
 	cfg.Producer.Retry.Max = 5
 	cfg.Producer.Return.Successes = true
 
-	flowConfig := config.GetConfig()
-	flowConfig.BootStrapServers = os.Getenv("OUTPUT_DATA_BOOTSTRAP_SERVERS")
+	outputConfig := config.GetConfig()
+	outputConfig.BootStrapServers = os.Getenv("OUTPUT_DATA_BOOTSTRAP_SERVERS")
+	outputConfig.KafkaTopic = os.Getenv("OUTPUT_DATA")
 
-
-	producer, err := sarama.NewSyncProducer([]string{flowConfig.BootStrapServers}, cfg)
+	producer, err := sarama.NewSyncProducer([]string{outputConfig.BootStrapServers}, cfg)
 	if err != nil {
 		// Should not reach here
 		panic(err)
@@ -110,15 +77,48 @@ func sendResponse(payload []byte) {
 		}
 	}()
 
-	msg := &sarama.ProducerMessage{
-		Topic: os.Getenv("OUTPUT_DATA"),
-		Value: sarama.StringEncoder(payload),
-	}
+ConsumerLoop:
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			//"http://172.30.143.13"
+			//helloworld-go.myproject.example.com
 
-	partition, offset, err := producer.SendMessage(msg)
-	if err != nil {
-		panic(err)
-	}
+			//Call the Serving function
+			req, err := http.NewRequest("POST", os.Getenv("URL"), bytes.NewBuffer(msg.Value))
+			req.Host = os.Getenv("HOST")
 
-	fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", flowConfig.KafkaTopic, partition, offset)
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Printf("Error sending to Serving function: %s", err)
+			}
+
+			defer resp.Body.Close()
+
+			fmt.Println("response Status:", resp.Status)
+			fmt.Println("response Headers:", resp.Header)
+
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			fmt.Println("response Body:", string(body))
+			consumed++
+
+			//Send the response downstream
+			outputMsg := &sarama.ProducerMessage{
+				Topic: outputConfig.KafkaTopic,
+				Value: sarama.StringEncoder(body),
+			}
+
+			partition, offset, err := producer.SendMessage(outputMsg)
+			if err != nil {
+				log.Printf("Error sending to Serving Kafka output: %s", err)
+			}
+
+			fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", outputConfig.KafkaTopic, partition, offset)
+
+		case <-signals:
+			break ConsumerLoop
+		}
+	}
 }
